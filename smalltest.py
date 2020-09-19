7b1b7d511453b2bfbb21be47abfcf4f6c1622f51
Burke <rbg18@pitt.edu>
2020-09-19 16:31:48 -0400

    PEP8 style changes, consolidated/removed broken code from bad Pymatgen import/init, made lmp_files for all data

--- smalltest.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import numpy as np
-from pymatgen.optimization.neighbors import find_points_in_spheres
-with open('data.p', 'rb') as f:
-    data = pickle.load(f)
-
-result = find_points_in_spheres(center_coords = data['first'], all_coords = data['second'], r = 3, pbc = np.array([1,1,1]), lattice = data['cell'])
-print(result)
